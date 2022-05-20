9b9b42f872b850ff48e345ae8cf66e773a3ac7c4
JunKaiYou <105905416+JunKaiYou@users.noreply.github.com>
2022-05-20 17:53:29 +0800

    Add files via upload

---
+++ Conditional_Sandwiched_Renyi_Entropy.m
@@ -0,0 +1,257 @@
+clear all; close all;
+% Change to the path where QETLAB and MFT toolboxes you downloaded
+% If you have already added the toolboxes into your library directory, you
+% don't need the following two lines.
+addpath(genpath('C:\EXP\QETLAB-0.9'))
+addpath(genpath('C:\EXP\mft\mftoolbox'))
+%
+
+%% Random density matrix 
+d_A = 64; d_B = 10; random_num = 1;
+rho_AB = RandomDensityMatrix(d_A * d_B);
+I_A = eye(d_A);
+
+
+%% Parameters
+max_iter_minimizer = 20;
+max_iter = 10;
+alpha = 10; 
+
+% for Armijo 
+Armijo_params.alpha_mirror = 8; 
+Armijo_params.r = 0.6;   
+Armijo_params.tau = 0.6;
+
+% for Polyak 1
+Polyak1_params.delta_t = 1;
+Polyak1_params.delta = 0.00001;
+Polyak1_params.rho = 1.3; 
+Polyak1_params.beta = 0.7; 
+
+% for Polyak 2
+Polyak2_params.delta_l = 1;
+Polyak2_params.B = 0.5;
+
+path = 0; tl = 1; l = 1; % Polyak 2 
+
+sigma0 = eye(d_B)./d_B; % initial point
+
+%% minimizer
+
+sigma_B = sigma0;
+for i = 1 : max_iter_minimizer 
+    X = ['Armijomin', num2str(i)];
+    disp(X);
+    func_grad = grad(sigma_B , alpha, I_A, rho_AB, d_A, d_B);
+    [sigma_B] = Armijo(sigma_B, func_grad, alpha, rho_AB, I_A, Armijo_params);
+end
+
+f_min = func_val(sigma_B, alpha, I_A, rho_AB); % minimizer
+
+%% Algorithms
+
+% Armijo
+seq_mirror = func_val(sigma0, alpha, I_A, rho_AB) - f_min;
+seq_t_mirror = 0;
+sigma_B = sigma0;
+for i = 1 : max_iter
+    X = ['Armijo', num2str(i)];
+    disp(X);
+    tic
+    func_grad = grad(sigma_B , alpha, I_A, rho_AB,d_A, d_B) ;
+    [sigma_B] = Armijo(sigma_B, func_grad, alpha, rho_AB, I_A, Armijo_params);
+    t_mirror = toc;
+    
+    seq_mirror= [seq_mirror real(func_val(sigma_B, alpha, I_A, rho_AB)- f_min)];
+    seq_t_mirror = [seq_t_mirror, seq_t_mirror(i) + t_mirror];
+end
+seq_mirror = real(seq_mirror);
+
+% Polyak 1
+seq_Polyak = func_val(sigma0, alpha, I_A, rho_AB)- f_min;
+seq_t_Polyak = 0;
+
+f_min_temp1 = func_val(sigma0, alpha, I_A, rho_AB);
+sigma_B = sigma0;
+
+for i = 1 : max_iter 
+    X = ['Polyak', num2str(i)];
+    disp(X);
+    tic
+    func_grad = grad(sigma_B , alpha, I_A, rho_AB, d_A, d_B);
+    [sigma_B, Polyak1_params.delta_t] = Polyak1(sigma_B, func_grad, alpha, rho_AB, f_min_temp1, I_A, Polyak1_params);
+    t_Polyak = toc;
+    
+    seq_Polyak = [seq_Polyak real(func_val(sigma_B, alpha, I_A, rho_AB)- f_min)];
+    f_min_temp1 = min(f_min_temp1, func_val(sigma_B, alpha, I_A, rho_AB)); 
+    seq_t_Polyak = [seq_t_Polyak, seq_t_Polyak(i) + t_Polyak];
+end
+seq_Polyak = real(seq_Polyak);
+
+% Polyak 2
+seq_Polyak2 = func_val(sigma0, alpha, I_A, rho_AB)- f_min;
+seq_t_Polyak2 = 0;
+seq_Polyak_main2 =  func_val(sigma0, alpha, I_A, rho_AB);
+sigma_B = sigma0;
+
+for i = 1 : max_iter
+    X = ['Polyak2', num2str(i)];
+    disp(X);
+    tic
+    func_grad = grad(sigma_B , alpha, I_A, rho_AB, d_A, d_B);
+    [sigma_B, Polyak2_params.delta_l, path, tl, l] = Polyak2(sigma_B, func_grad, alpha, I_A, rho_AB, seq_Polyak_main2, i, path, tl, l, Polyak2_params);
+    t_Polyak2 = toc;
+    
+    seq_Polyak2 = [seq_Polyak2 real(func_val(sigma_B, alpha, I_A, rho_AB)- f_min)];
+    seq_Polyak_main2 = [seq_Polyak_main2 func_val(sigma_B, alpha, I_A, rho_AB)];
+    
+    seq_t_Polyak2 = [seq_t_Polyak2, seq_t_Polyak2(i) + t_Polyak2];
+end
+seq_Polyak2 = real(seq_Polyak2);
+
+% do not consider numerical errors whose values are smaller than 10^(-5)
+I = find(seq_mirror < 10^(-5), 1);
+seq_mirror(:, I+1:end) = [];
+I2 = find(seq_Polyak < 10^(-5), 1);
+seq_Polyak(:, I2+1:end) = [];
+I3 = find(seq_Polyak2 < 10^(-5), 1);
+seq_Polyak2(:, I3+1:end) = [];
+
+%% f-f_min v.s iteration
+figure(1)
+lw = 'linewidth';
+linewidth = 2;
+subplot(2,1,1)
+semilogy(seq_Polyak, lw, linewidth)
+hold on
+semilogy(seq_Polyak2, lw, linewidth)
+semilogy(real(seq_mirror), lw, linewidth)
+ylim([10^(-5), max([max(real(seq_mirror)), max(real(seq_Polyak)), max(real(seq_Polyak2))])])
+yticks([10^(-5) 10^(-4) 10^(-2)])
+
+grid on
+xlim([1 max([length(seq_mirror), length(seq_Polyak), length(seq_Polyak2)])])
+xlabel('number of iterations')
+ylabel('f(x_t)-f*')
+legend("Polyak","Polyak2", "Armijo")
+hold off
+
+%% f-f_min v.s elapsed time
+
+subplot(2,1,2)
+lw = 'linewidth';
+linewidth = 2;
+semilogy(seq_t_Polyak(:,1:length(seq_Polyak)), seq_Polyak, lw, linewidth)
+
+hold on
+semilogy(seq_t_Polyak2(:,1:length(seq_Polyak2)), seq_Polyak2, lw, linewidth)
+semilogy(seq_t_mirror(:,1:length(seq_mirror)), seq_mirror, lw, linewidth)
+ylim([10^(-5), max([max(real(seq_mirror)), max(real(seq_Polyak)), max(real(seq_Polyak2))])])
+yticks([10^(-5) 10^(-4) 10^(-2)])
+
+grid on
+xlabel('Elapsed time (s)')
+ylabel('f(x_t)-f*')
+legend( "Polyak","Polyak2","Armijo")
+hold off
+
+function [output,delta_t] = Polyak1(sigma_B, func_grad, a, rho_AB, f_min_temp1, I_A, Polyak1_params)
+    
+    delta = Polyak1_params.delta; rho = Polyak1_params.rho; 
+    beta = Polyak1_params.beta; delta_t = Polyak1_params.delta_t;
+    
+    f_t = func_val(sigma_B, a, I_A, rho_AB);
+    f_min =  f_min_temp1;
+    
+    schattern_infi = abs(eigs(func_grad,1));
+    
+    eta =  (f_t - (f_min - delta_t))/(schattern_infi^2);
+    
+    x_alpha = expm(logm(sigma_B) - eta * func_grad);
+    x_alpha = x_alpha / trace(x_alpha);
+    
+    if func_val(x_alpha, a, I_A, rho_AB) <= f_min - delta_t
+        delta_t = rho * delta_t;
+    else
+        delta_t = max(beta * delta_t, delta);
+    end
+   
+    output = x_alpha;
+end
+
+function [output, delta_l, path, tl, l] = Polyak2(sigma_B, func_grad, a, I_A, rho_AB, seq_Polyak_main, iter, path, tl, l, Polyak2_params)
+    
+    B = Polyak2_params.B; delta_l = Polyak2_params.delta_l;
+    
+    f_t = func_val(sigma_B, a, I_A, rho_AB);
+    f_min_tl =  min(seq_Polyak_main(1:tl(l)));
+    
+    if f_t <= f_min_tl - delta_l/2
+        tl = [tl, iter]; path = 0; l = l+1;
+        f_min_tl =  min(seq_Polyak_main(1:tl(l)));
+        
+        schattern_infi = abs(eigs(func_grad,1));  
+        eta =  (f_t - (f_min_tl - delta_l))/(schattern_infi^2);
+    
+        x_alpha = expm(logm(sigma_B) - eta * func_grad);
+        x_alpha = x_alpha / trace(x_alpha);
+        
+        path = path + eta * schattern_infi;
+    else
+        if path > B
+            tl = [tl, iter]; path = 0; delta_l = delta_l/2;
+            l = l+1; f_min_tl =  min(seq_Polyak_main(1:tl(l)));
+            
+            schattern_infi = abs(eigs(func_grad,1));   
+            eta =  (f_t - (f_min_tl - delta_l))/(schattern_infi^2);
+    
+            x_alpha = expm(logm(sigma_B) - eta * func_grad);
+            x_alpha = x_alpha / trace(x_alpha);
+        
+            path = path + eta * schattern_infi;
+        else
+            schattern_infi = abs(eigs(func_grad,1));  
+            eta =  (f_t - (f_min_tl - delta_l))/(schattern_infi^2);
+    
+            x_alpha = expm(logm(sigma_B) - eta * func_grad);
+            x_alpha = x_alpha / trace(x_alpha);
+        
+            path = path + eta * schattern_infi;
+        end
+    end    
+    output = x_alpha;
+end
+
+function [output] = Armijo(sigma_B, func_grad, a, rho_AB, I_A, Armijo_params)
+    
+    alpha_mirror = Armijo_params.alpha_mirror;
+    r = Armijo_params.r;
+    tau = Armijo_params.tau;
+    do = true;
+    
+    while do
+        x_alpha = expm(logm(sigma_B) - alpha_mirror * func_grad);
+        x_alpha = x_alpha / trace(x_alpha);
+        do = tau * trace(func_grad * ((x_alpha - sigma_B)'))+ func_val(sigma_B , a, I_A, rho_AB) <= func_val(x_alpha , a, I_A, rho_AB) - 1e-15;
+
+        alpha_mirror = alpha_mirror * r;
+    end
+    output = x_alpha;
+end
+
+% Cauculate the objecive function value
+function output = func_val(sigma_B , a, I_A, rho_AB) 
+    s = (1-a)/a;
+    output = log(trace((Tensor(I_A,sigma_B)^(s/2)*rho_AB*Tensor(I_A,sigma_B)^(s/2))^a))/(a-1);
+end
+
+% Cauculate the gradient of the objecive function 
+function output = grad(sigma_B , a, I_A, rho_AB, d_A, d_B) 
+    s = (1-a)/a;
+    sigma_B_temp = sigma_B ^(-s/2);
+    temp = (Tensor(I_A,sigma_B)^(s/2)*rho_AB*Tensor(I_A,sigma_B)^(s/2))^a;
+    temp_partial = PartialTrace(temp, 1, [d_A, d_B]);
+    [~,Frechet] = powerm_pade_fre( sigma_B, s, temp_partial);
+    output = sigma_B_temp * Frechet * sigma_B_temp /trace(temp);
+    output = output.*(a/(a-1));
+end
